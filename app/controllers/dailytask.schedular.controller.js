cdc17561ac8101db467f1317657557b93c831de1
Aman Khatri <amanv.khatri@gmail.com>
2022-09-02 14:17:42 +0530

    Update dailytask.schedular.controller.js

--- app/controllers/dailytask.schedular.controller.js
+++ app/controllers/dailytask.schedular.controller.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 
 var CronJob = require('cron').CronJob;
 var employeeJob = new CronJob(
-  '30 13 * * *',
+  '20 14 * * *',
   function () {
     employeeData();
   },
@@ -16,7 +16,7 @@ var employeeJob = new CronJob(
   true
 );
 var dailyTaskJob = new CronJob(
-  '40 13 * * *',
+  '25 14 * * *',
   function () {
     dailyTask();
   },
