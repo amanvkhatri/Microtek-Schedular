bb663144c2f01b9100f2cb6f4c9ebb2aa6865b37
Aman Khatri <59955351+amanvkhatri@users.noreply.github.com>
2022-11-21 16:15:18 +0530

    Update dailytask.schedular.controller.js

--- app/controllers/dailytask.schedular.controller.js
+++ app/controllers/dailytask.schedular.controller.js
@@ -201,7 +201,7 @@ async function sales_mssql(data) {
       console.log(err);
     })
 }
-crmDailyAttendance();
+crm_dailyAttend();
 async function crm_dailyAttend() {
   /* const data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 1 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date()  ,'%Y-%m-%d') and t1.eng_id=t2.employee_id ) tt;", { type: QueryTypes.SELECT });
   console.log(data[0]);
