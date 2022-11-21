2b49923d571294e6e5bd64c850330057384fa278
Aman Khatri <59955351+amanvkhatri@users.noreply.github.com>
2022-11-21 16:12:51 +0530

    Update dailytask.schedular.controller.js

--- app/controllers/dailytask.schedular.controller.js
+++ app/controllers/dailytask.schedular.controller.js
@@ -201,10 +201,23 @@ async function sales_mssql(data) {
       console.log(err);
     })
 }
+crmDailyAttendance();
 async function crm_dailyAttend() {
-  const data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 1 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date()  ,'%Y-%m-%d') and t1.eng_id=t2.employee_id ) tt;", { type: QueryTypes.SELECT });
+  /* const data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 1 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date()  ,'%Y-%m-%d') and t1.eng_id=t2.employee_id ) tt;", { type: QueryTypes.SELECT });
   console.log(data[0]);
   data?.map(attend => {
+    crm_mssql(attend);
+    console.log(attend);
+    crmDailyAttendance.create(attend)
+      .then(data => {
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }) */
+  const unmatched_data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 9999999 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date()  ,'%Y-%m-%d') and t1.eng_id Not In (select distinct(employee_id) from crm_employee_mapping) ) tt;", { type: QueryTypes.SELECT });
+  console.log(unmatched_data[0]);
+  unmatched_data?.map(attend => {
     crm_mssql(attend);
     console.log(attend);
     crmDailyAttendance.create(attend)
