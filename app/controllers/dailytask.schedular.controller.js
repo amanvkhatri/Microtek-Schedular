5a531bd76e5f24401f3faf46a732ce3f82aa55ba
Aman Khatri <59955351+amanvkhatri@users.noreply.github.com>
2022-12-15 13:17:00 +0530

    Update dailytask.schedular.controller.js

--- app/controllers/dailytask.schedular.controller.js
+++ app/controllers/dailytask.schedular.controller.js
@@ -90,7 +90,6 @@ async function storeEmployee(emp) {
       console.log(err);
     })
 }
-dailyTask();
 function dailyTask() {
   console.log("hello");
   erpRecord.findAll()
@@ -162,10 +161,11 @@ function getMomentDate() {
   const final_date = month + "/" + day + "/" + year;
   return (final_date);
 }
+sales_dailyAttend();
 async function sales_dailyAttend() {
-  const data = await sequelize.query("select employee_id, InTime, OutTime from ( select daystarttype DayStartType,date_format(InTime,'%Y-%m-%d') Date,UserErpId,PunchDate, sales_mst.new_e_code employee_id, min(date_add(case when ActivityType='Day End (Normal)' then OutTime else  InTime end,INTERVAL 330 minute)) InTime, max(date_add(case when ActivityType='Day Start' then InTime else  OutTime end,INTERVAL 330 minute)) OutTime from dailytasks as tasks, sales_employee_mapping as sales_mst where tasks.UserErpId = sales_mst.sales_id and InTime >date_format(current_date() - INTERVAL 1 DAY ,'%Y-%m-%d') and InTime < date_format(current_date(),'%Y-%m-%d') group by daystarttype,PunchDate,UserErpId order by daystarttype,usererpid,PunchDate) tt;", { type: QueryTypes.SELECT });
+  const data = await sequelize.query("select employee_id, InTime, OutTime from ( select daystarttype DayStartType,date_format(InTime,'%Y-%m-%d') Date,UserErpId,PunchDate, sales_mst.new_e_code employee_id, min(date_add(case when ActivityType='Day End (Normal)' then OutTime else  InTime end,INTERVAL 330 minute)) InTime, max(date_add(case when ActivityType='Day Start' then InTime else  OutTime end,INTERVAL 330 minute)) OutTime from dailytasks as tasks, sales_employee_mapping as sales_mst where tasks.UserErpId = sales_mst.sales_id and InTime >date_format(current_date() - INTERVAL 14 DAY ,'%Y-%m-%d') and InTime < date_format(current_date(),'%Y-%m-%d') group by daystarttype,PunchDate,UserErpId order by daystarttype,usererpid,PunchDate) tt;", { type: QueryTypes.SELECT });
   data?.map(async attend => {
-    //sales_mssql(attend);
+    sales_mssql(attend);
     console.log(attend);
     console.log(`${attend.InTime}`);
     await sequelize.query(`Insert into sales_dailyattendances (employee_id, InTime, OutTime, createdAt, updatedAt) values ('${attend.employee_id}','${attend.InTime?.toISOString().slice(0, 19).replace('T', ' ')}', '${attend.OutTime?.toISOString().slice(0, 19).replace('T', ' ')}',now(),now())`, { type: QueryTypes.INSERT })
@@ -182,9 +182,9 @@ async function sales_dailyAttend() {
         console.log(err);
       }) */
   })
-  const unmatched_data = await sequelize.query(`select UserErpId as employee_id, min(date_add(case when ActivityType='Day End (Normal)' then OutTime else  InTime end,INTERVAL 330 minute)) InTime, max(date_add(case when ActivityType='Day Start' then InTime else  OutTime end,INTERVAL 330 minute)) OutTime from dailytasks as tasks where InTime >date_format(current_date() - INTERVAL 1 DAY ,'%Y-%m-%d') and InTime < date_format(current_date(),'%Y-%m-%d') and UserErpId Not In (select distinct(sales_id) from sales_employee_mapping) group by daystarttype,PunchDate,UserErpId order by daystarttype,usererpid,PunchDate;`, { type: QueryTypes.SELECT });
+  const unmatched_data = await sequelize.query(`select UserErpId as employee_id, min(date_add(case when ActivityType='Day End (Normal)' then OutTime else  InTime end,INTERVAL 330 minute)) InTime, max(date_add(case when ActivityType='Day Start' then InTime else  OutTime end,INTERVAL 330 minute)) OutTime from dailytasks as tasks where InTime >date_format(current_date() - INTERVAL 14 DAY ,'%Y-%m-%d') and InTime < date_format(current_date(),'%Y-%m-%d') and UserErpId Not In (select distinct(sales_id) from sales_employee_mapping) group by daystarttype,PunchDate,UserErpId order by daystarttype,usererpid,PunchDate;`, { type: QueryTypes.SELECT });
   unmatched_data?.map(async attend => {
-    //sales_mssql(attend);
+    sales_mssql(attend);
     console.log(attend);
     await sequelize.query(`Insert into sales_dailyattendances (employee_id, InTime, OutTime, createdAt, updatedAt) values ('${attend.employee_id}','${attend.InTime?.toISOString().slice(0, 19).replace('T', ' ')}', '${attend.OutTime?.toISOString().slice(0, 19).replace('T', ' ')}',now(),now())`, { type: QueryTypes.INSERT })
       .then((res) => {
@@ -227,8 +227,9 @@ async function sales_mssql(data) {
       console.log(err);
     })
 }
+crm_dailyAttend
 async function crm_dailyAttend() {
-  const data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 1 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date(),'%Y-%m-%d') and t1.eng_id=t2.employee_id ) tt;", { type: QueryTypes.SELECT });
+  const data = await sequelize.query("select new_e_code employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1,crm_employee_mapping t2 where  t1.punch_date >=date_format(current_date() -  INTERVAL 14 DAY,'%Y-%m-%d') and t1.punch_date < date_format(current_date(),'%Y-%m-%d') and t1.eng_id=t2.employee_id ) tt;", { type: QueryTypes.SELECT });
   console.log(data[0]);
   data?.map(async attend => {
     crm_mssql(attend);
@@ -249,7 +250,7 @@ async function crm_dailyAttend() {
         console.log(err);
       }) */
   })
-  const unmatched_data = await sequelize.query(`select eng_id employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1 where  t1.punch_date >=date_format(current_date() -  INTERVAL 1 DAY,'%Y-%m-%d') and  t1.punch_date < date_format(current_date(),'%Y-%m-%d') and  t1.eng_id Not In (select distinct(employee_id) from crm_employee_mapping) ) tt;`, { type: QueryTypes.SELECT });
+  const unmatched_data = await sequelize.query(`select eng_id employee_id,in_time InTime, out_time OutTime from ( select * from attendancedata t1 where  t1.punch_date >=date_format(current_date() -  INTERVAL 14 DAY,'%Y-%m-%d') and  t1.punch_date < date_format(current_date(),'%Y-%m-%d') and  t1.eng_id Not In (select distinct(employee_id) from crm_employee_mapping) ) tt;`, { type: QueryTypes.SELECT });
   console.log(unmatched_data[0]);
   unmatched_data?.map(async attend => {
     crm_mssql(attend);
