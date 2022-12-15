e7afe3081e39a8b7543cc1d3ea79aeeb8ae3198f
Aman Khatri <59955351+amanvkhatri@users.noreply.github.com>
2022-12-15 12:34:03 +0530

    Update dailytask.schedular.controller.js

--- app/controllers/dailytask.schedular.controller.js
+++ app/controllers/dailytask.schedular.controller.js
@@ -102,7 +102,7 @@ function dailyTask() {
     })
 }
 async function getTask(id) {
-  const date = '12/04/2022'//getMomentDate(); // mm/dd/yyyy
+  const date = '12/05/2022'//getMomentDate(); // mm/dd/yyyy
   const datearray = date.split("/");
   const sqlDate = datearray[2] + "-" + datearray[0] + "-" + datearray[1]
   console.log(sqlDate);
